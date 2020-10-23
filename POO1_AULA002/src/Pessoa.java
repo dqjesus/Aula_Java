bd9a55a0c13616fbf12c79f3b2a1e355ee5b901b
Daniel Quintale de Jesus <dqjesus@gmail.com>
2020-10-23 15:13:39 -0300

    Aula 02 - Criação de Classe e Instanciar Objeto

---
+++ POO1_AULA002/src/Pessoa.java
@@ -0,0 +1,10 @@
+//Criar uma classe pessoa com 5 atributos
+public class Pessoa {
+	
+	public String Nome;
+	public String Sexo;
+	public int Idade;
+	public double Altura;
+	public double Peso;
+	
+}
