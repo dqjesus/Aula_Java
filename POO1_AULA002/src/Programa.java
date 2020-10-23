e9b944ea9fe62731b216512240c518a87be3501f
Daniel Quintale de Jesus <dqjesus@gmail.com>
2020-10-23 15:18:32 -0300

    Aula 02 - Instancia Objeto

--- POO1_AULA002/src/Programa.java
+++ POO1_AULA002/src/Programa.java
@@ -3,7 +3,23 @@ public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Pessoa objPessoa = new Pessoa();
+		
+		objPessoa.Nome = "Daniel";
+		objPessoa.Sexo = "Masculino";
+		objPessoa.Idade = 38;
+		objPessoa.Altura = 1.63;
+		objPessoa.Peso = 72.5;
+		
+		System.out.println("Nome: " + objPessoa.Nome);
+		System.out.println("Sexo: " + objPessoa.Sexo);
+		System.out.println("Idade: " + objPessoa.Idade);
+		System.out.println("Altura: " + objPessoa.Altura);
+		System.out.println("Peso: " + objPessoa.Peso);
+		
+		
+		
 	}
 
 }
