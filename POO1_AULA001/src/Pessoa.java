44238e5bcf5958bba553caff6c0e370dcc45d89b
dqjesus <73341667+dqjesus@users.noreply.github.com>
2021-10-28 20:21:50 -0300

    Update Pessoa.java
    TEste 28/10

--- POO1_AULA001/src/Pessoa.java
+++ POO1_AULA001/src/Pessoa.java
@@ -1,10 +1,14 @@
 //Criar uma classe pessoa com 5 atributos
 public class Pessoa {
 	
-	public String Nome;
+	private String Nome;
 	public String Sexo;
 	public int Idade;
 	public double Altura;
 	public double Peso;
 	
+	public void setNome (String Nome)
+	{
+		this.Nome = Nome;
+	}
 }
